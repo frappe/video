7ea5076a894d7a44377828ea8ac340d0ba46773d
Sagar Vora <sagar@erpnext.com>
2017-05-15 11:49:32 +0530

    Remove node_modules from git

--- node_modules/testcafe-browser-tools/bin/linux/find-window.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xwininfo -tree -root | grep -i "$1" | sed -e "s/^\s*\(0x[0-9,A-H]\+\).*/\1/ip" -n
--- node_modules/testcafe/lib/test-run/client-messages.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-exports.__esModule = true;
-exports.default = {
-    ready: 'ready',
-    readyForBrowserManipulation: 'ready-for-browser-manipulation',
-    waitForFileDownload: 'wait-for-file-download'
-};
-module.exports = exports['default'];
