a8eb8cbd37356bb74172d6985c84bda4058f57bf
Umair Sayyed <umair@erpnext.com>
2017-08-04 19:03:06 +0530

    fixes

--- videos/tree_master.js
+++ videos/tree_master.js
@@ -13,93 +13,93 @@ test('Tree master in ERPNext', async t => {
 
     await f.toggle_speak(true)
 
- 	//     await f.speak
- 	//       	  (`
- 	// 		Hi. Welcome to E R P Next video tutorial. In this video, we will learn how to manage masters which are maintained
- 	// 		  in the tree structure. Few of the important tree structured masters are Chart of Accounts, Warehouse, Item Group etc.
- 	// 		  `)
- 	//
- 	// //chart of accounts
- 	//
- 	// await f.search("chart of ac ", "Open Chart of Accounts")
- 	//
- 	// await f.speak
- 	//       	  (`
- 	// 		In the Chart of Accounts master, Account is the most parent account.
- 	// 		  `)
- 	//
- 	// var link = await f.select_element('.tree-label', 'Accounts')
- 	//
- 	// await f.click(link)
- 	//
- 	// await f.speak
- 	//       (`This parent has following sub-groups under it.`)
- 	//
- 	// var link = await f.select_element('a', 'Application of Funds (Assets) - GTPL')
- 	//
- 	// await f.click(link)
- 	//
- 	// var link = await f.select_element('a', 'Source of Funds (Liabilities) - GTPL')
- 	//
- 	// await f.click(link)
- 	//
- 	// var link = await f.select_element('a', 'Income - GTPL')
- 	//
- 	// await f.click(link)
- 	//
- 	// var link = await f.select_element('a', 'Expenses - GTPL')
- 	//
- 	// await f.click(link)
- 	//
- 	// await f.speak
- 	//       (`Under each group, you have multiple sub-groups and child accounts.`)
- 	//
- 	// var link = await f.select_element('a', 'Current Assets - GTPL')
- 	//
- 	// await f.click(link)
- 	//
- 	// var link = await f.select_element('a', 'Bank Accounts - GTPL')
- 	//
- 	// await f.click(link)
- 	//
- 	// await f.speak
- 	//       (`Following are the child Accounts which are selected in the transactions.`)
- 	//
- 	// await f.hover_element('a', 'Bank of America - GTPL', 1)
- 	// await f.hover_element('a', 'HDFC Bank - GTPL', 1)
- 	// await f.hover_element('a', 'Standard Bank - GTPL', 1)
- 	//
- 	// await f.speak
- 	//       (`To add new child account in this parent, click on the group, and Add Child.`)
- 	//
- 	// var link = await f.select_element('a', 'Bank Accounts - GTPL')
- 	//
- 	// await f.click(link)
- 	//
- 	// var link = await f.select_element('.btn', 'Add Child')
- 	//
- 	// await f.click(link)
- 	//
- 	// await f.speak
- 	//       (`Enter name of new account.`)
- 	//
- 	// await f.fill_field("New Account Name", "HSBC Bank")
- 	//
- 	// await f.hover_element("label","Is Group")
- 	//
- 	// await f.speak
- 	//       (`Check Is Group if you want to add this Account as a group.`)
- 	//
- 	// await f.fill_field("Account Type", "Bank")
- 	//
- 	// await f.click_primary_button()
- 	//
- 	// var link = await f.select_element('a', 'HSBC Bank - GTPL')
- 	//
- 	// await f.click(link)
- 	//
- 	// await f.speak
- 	//       (`This is how you can create child account in the existing groups. Lets also check how to manage Warehouse master.`)
+ 	await f.speak
+ 	      	  (`
+ 			Hi. Welcome to E R P Next video tutorial. In this video, we will learn how to manage masters which are maintained
+ 			  in the tree structure. Few of the important tree structured masters are Chart of Accounts, Warehouse, Item Group etc.
+ 			  `)
+
+ 	//chart of accounts
+
+ 	await f.search("chart of ac ", "Open Chart of Accounts")
+
+ 	await f.speak
+ 	      	  (`
+ 			In the Chart of Accounts master, Account is the most parent account.
+ 			  `)
+
+ 	var link = await f.select_element('.tree-label', 'Accounts')
+
+ 	await f.click(link)
+
+ 	await f.speak
+ 	      (`This parent has following sub-groups under it.`)
+
+ 	var link = await f.select_element('a', 'Application of Funds (Assets) - GTPL')
+
+ 	await f.click(link)
+
+ 	var link = await f.select_element('a', 'Source of Funds (Liabilities) - GTPL')
+
+ 	await f.click(link)
+
+ 	var link = await f.select_element('a', 'Income - GTPL')
+
+ 	await f.click(link)
+
+ 	var link = await f.select_element('a', 'Expenses - GTPL')
+
+ 	await f.click(link)
+
+ 	await f.speak
+ 	      (`Under each group, you have multiple sub-groups and child accounts.`)
+
+ 	var link = await f.select_element('a', 'Current Assets - GTPL')
+
+ 	await f.click(link)
+
+ 	var link = await f.select_element('a', 'Bank Accounts - GTPL')
+
+ 	await f.click(link)
+
+ 	await f.speak
+ 	      (`Following are the child Accounts which are selected in the transactions.`)
+
+ 	await f.hover_element('a', 'Bank of America - GTPL', 1)
+ 	await f.hover_element('a', 'HDFC Bank - GTPL', 1)
+ 	await f.hover_element('a', 'Standard Bank - GTPL', 1)
+
+ 	await f.speak
+ 	      (`To add new child account in this parent, click on the group, and Add Child.`)
+
+ 	var link = await f.select_element('a', 'Bank Accounts - GTPL')
+
+ 	await f.click(link)
+
+ 	var link = await f.select_element('.btn', 'Add Child')
+
+ 	await f.click(link)
+
+ 	await f.speak
+ 	      (`Enter name of new account.`)
+
+ 	await f.fill_field("New Account Name", "HSBC Bank")
+
+ 	await f.hover_element("label","Is Group")
+
+ 	await f.speak
+ 	      (`Check Is Group if you want to add this Account as a group.`)
+
+ 	await f.fill_field("Account Type", "Bank")
+
+ 	await f.click_primary_button()
+
+ 	var link = await f.select_element('a', 'HSBC Bank - GTPL')
+
+ 	await f.click(link)
+
+ 	await f.speak
+ 	      (`This is how you can create child account in the existing groups. Lets also check how to manage Warehouse master.`)
  
  	await f.search("wareh", "Warehouse Tree")
 		
