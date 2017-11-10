f7e414fb87dc6d3fd61c98b472cb18acd109fff3
Umair Sayyed <umair@erpnext.com>
2017-11-10 15:26:52 +0530

    Fee structure

--- videos/fee_structure.js
+++ videos/fee_structure.js
@@ -25,14 +25,16 @@ test(video_title, async t => {
 
 	await f.add_slide({
 		title: "Fee Structure",
-		bullet_points: [ 'Fee Category', 'Fees Accounting' ],
+		bullet_points: [ 'Fee Category', 'Fee Accounts' ],
 			})
 
-	await f.speak(`Fee Structure is where you select fee component and define formula based on which student fee will be calculated.`)
+	await f.speak(`Fee Structure is composed of fee categories and amount based on which total fee is calculated. for the specific Program.`)
 
 	await f.remove_slide()
 
-	await f.speak(`Before creating a fee structure, we should create fee categories.`)
+	await f.speak(`Fee Structure is created for a specific Program and academic term.`)
+
+	await f.speak(`Before creating a fee structure, we need to create fee categories.`)
 
 	await f.click_icon("Schools")
 
@@ -44,7 +46,7 @@ test(video_title, async t => {
 
 	await f.click_primary_button("New")
 
-	await f.speak(`Enter name for fee category.`)
+	await f.speak(`Enter fee category name.`)
 
 	await f.fill_field("Name", "Library Fee")
 
@@ -54,29 +56,9 @@ test(video_title, async t => {
 
 	await f.speak(`This way, you can add more fee categories.`)
 
-	await f.speak(`Now, let's create Accounts which fee will be booked as an income.`)
+	await f.speak(`Now, lets create Accounts needed for creating fee structure.`)
 
 	await f.search("chart of ac", "Open Chart of Accounts")
-
-	await f.speak(`Let's create income account for fees collection.`)
-
-	await f.click_tree_node("Income - WC")
-
-	await f.click_tree_node("Direct Income - WC")
-
-	await f.click_tree_node_option("Add Child")
-
-	await f.fill_field("New Account Name", "Student Fee")
-
-	await f.fill_field("Account Type", "Income Account")
-
-	await f.click_primary_button("Create New")
-
-	await f.pause(1)
-
-	await f.click_tree_node("Student Fee - WC")
-
-	await f.speak(`Same way, we can also add fee receivable account.`)
 
 	await f.click_tree_node("Application of Funds (Assets) - WC")
 
@@ -95,6 +77,14 @@ test(video_title, async t => {
 	await f.pause(1)
 
 	await f.click_tree_node("Fee Receivable - WC")
+
+	await f.speak(`Same way, we can also add an income account in which fee income will be booked.`)
+
+	await f.click_tree_node("Income - WC")
+
+	await f.click_tree_node("Direct Income - WC")
+
+	await f.click_tree_node("Student Fee - WC")
 
 	await f.speak(`Now, we can create a Fee Structure.`)
 
@@ -175,27 +165,29 @@ test(video_title, async t => {
 	await f.click_primary_button("Yes")
 
 	await f.speak(`Once Fee Structure is created, you can link it in various other masters.`)
-		
+
+	await f.pause(4)
+	
 	await f.search("program", "Program List")
-		
+
 	await f.click_list_item("BCS-FY")
-		
+
 	await f.scroll_to_section("Fee Schedule")
-		
+
 	await f.add_row("fees")
-		
+
 	await f.open_row("fees", 1)
-		
-	await f.fill_field("Fee Structure", "FS00010", "10")
-		
+
+	await f.fill_field("Fee Structure", "FS00011", "11")
+
 	await f.close_row()
-		
+
 	await f.click_primary_button("Save")
-		
+
 	await f.speak(`With this, this Fee Structure will be applicable to all the students who enroll for this program.
 			Based on fee structure, Fee schedule and Fee receipt will be created for Students.
 		`)
-	
+
 	await f.speak(`
 			Hope you found this video tutorial useful. For more details and free sign up, check e r p next dot com.
 			Thanks for watching.
